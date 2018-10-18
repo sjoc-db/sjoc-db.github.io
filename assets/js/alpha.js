c4b7907af48963063ac426df041d5fed21f7f038
Edo Setiawan <edo@fourhatmarketing.com>
2018-10-18 18:11:15 +0700

    added 1960-70s characters, added modal

--- assets/js/alpha.js
+++ assets/js/alpha.js
@@ -14,10 +14,28 @@ $(document).ready(function(){
             });
             catsClassString = catsClass.join(" ");
             htmlcode = '<div class="'+catsClassString+'">';
-            htmlcode += '<a href="#"><img src="'+value.image_path+'" class="img img-fluid"></a>';
+            htmlcode += '<a href="#"><img src="'+value.image_portrait_path+'" class="img img-fluid" data-toggle="modal" data-target="#exampleModal" data-id="'+index+'"></a>';
             htmlcode += '<p class="name sr-only">'+value.name+'</p>';
+            htmlcode += '<p class="series sr-only">'+value.series+'</p>';
             htmlcode += '</div>'
             $('#characters').append(htmlcode);
         })
     })
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var id = button.data('id') // Extract info from data-* attributes
+        var modal = $(this)
+        $.getJSON('characters.json', function(data) {
+            var _name = data[id].name;
+            var _type = data[id].type;
+            var _class = data[id].class;
+            var _era = data[id].era;
+            var _categories = data[id].categories;
+            modal.find('.modal-title').text(_name)
+            $.each(_categories, function(index, category) {
+                modal.find('.modal-footer>.modal-categories').append("<span class='category-pill'>"+category+"</span>")
+            })
+        })
+      })
 })
